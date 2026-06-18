4c53e68ec4f5e63a0577fc7ace525f4c01ba8360
Sanghavi Kartigayan <sanghavikartigayan@gmail.com>
2026-06-18 19:24:17 -0400

    adding progress bar to the quiz

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,6 +1,7 @@
-import { useState } from "react";
+import { useState, useCallback } from "react";
 import questions from "../questions.jsx";
 import quizCompletedLogo from "../assets/quiz-complete.png";
+import QuestionTimer from "./QuestionTimer.jsx";
 
 const Quiz = () => {
   const [userAnswers, setUserAnswers] = useState([]);
@@ -9,11 +10,16 @@ const Quiz = () => {
 
   const quizIsComplete = activeQuestionIndex === questions.length;
 
-  const handleSelectAnswer = (selectedAnser) => {
+  const handleSelectAnswer = useCallback((selectedAnser) => {
     setUserAnswers((prevAnswers) => {
       return [...prevAnswers, selectedAnser];
     });
-  };
+  }, []); // useCallback is used here because it is this function that is added as a dependency to the handleSkipAnswer callback function.
+
+  const handleSkipAnswer = useCallback(
+    () => handleSelectAnswer(answer),
+    [handleSelectAnswer],
+  ); // useCallback is used here instead of the normal event function hanlder because this function will get recreated each time the timer component gets resetted. To avoid that issue we are caching this function using useCallbacks.
 
   if (quizIsComplete) {
     return (
@@ -30,13 +36,16 @@ const Quiz = () => {
   return (
     <div id="quiz">
       <div id="question">
+        <QuestionTimer
+          key={activeQuestionIndex} // adding key prop to this component here because this component will not get recreated or unmounted and remounted to the dom whenever the question changes. Adding the activeQuestionIndex as a key will help react to rerender this component as the index is now changed.
+          onTimeout={() => handleSelectAnswer(null)}
+          timeout={10000}
+        />
         <h2>{questions[activeQuestionIndex].text}</h2>
         <ul id="answers">
           {shuffleAnswers.map((answer) => (
             <li key={answer} className="answer">
-              <button onClick={() => handleSelectAnswer(answer)}>
-                {answer}
-              </button>
+              <button onClick={handleSkipAnswer}>{answer}</button>
             </li>
           ))}
         </ul>
