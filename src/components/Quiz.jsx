0f9ccfdf372f7255ca3ef1f9f10bf1014f502f9e
Sanghavi Kartigayan <sanghavikartigayan@gmail.com>
2026-06-18 18:43:00 -0400

    shuffle answers array to the user and conditionally show a completed screen to the user

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,10 +1,13 @@
 import { useState } from "react";
 import questions from "../questions.jsx";
+import quizCompletedLogo from "../assets/quiz-complete.png";
 
 const Quiz = () => {
   const [userAnswers, setUserAnswers] = useState([]);
 
   const activeQuestionIndex = userAnswers.length;
+
+  const quizIsComplete = activeQuestionIndex === questions.length;
 
   const handleSelectAnswer = (selectedAnser) => {
     setUserAnswers((prevAnswers) => {
@@ -12,12 +15,24 @@ const Quiz = () => {
     });
   };
 
+  if (quizIsComplete) {
+    return (
+      <div id="summary">
+        <img src={quizCompletedLogo} alt="Trophy Cup" />
+        <h2>Quiz Completed!</h2>
+      </div>
+    );
+  }
+
+  const shuffleAnswers = [...questions[activeQuestionIndex].answers];
+  shuffleAnswers.sort(() => Math.random() - 0.5); // Math random will give us a number between 0 and 1 and when we subtract 0.5 from it we get negative value. Negative value means it will be swapped. We are randomly shuffling the answers array to the user because the array always has the first element as the correct answer and we dont want to show like that to the user.
+
   return (
     <div id="quiz">
       <div id="question">
         <h2>{questions[activeQuestionIndex].text}</h2>
         <ul id="answers">
-          {questions[activeQuestionIndex].answers.map((answer) => (
+          {shuffleAnswers.map((answer) => (
             <li key={answer} className="answer">
               <button onClick={() => handleSelectAnswer(answer)}>
                 {answer}
